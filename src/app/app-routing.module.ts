4e8ff1407f19fc7727d5e8db454985e69ddaedc2
ksp220798 <ksp220798@gmail.com>
2024-10-19 14:01:29 +0530

    routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { MyfirstComponentComponent } from './myfirst-component/myfirst-component.component';
+import { AboutComponentComponent } from './about-component/about-component.component';
+const routes: Routes = [
+  {
+    path:'',redirectTo:'/home',pathMatch:'full'
+  },
+  { path: 'home', component: MyfirstComponentComponent },
+  { path: 'about', component: AboutComponentComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MyfirstComponentComponent } from './myfirst-component/myfirst-component.component';
 import { HttpClientModule } from '@angular/common/http';
+import { AboutComponentComponent } from './about-component/about-component.component';
 @NgModule({
   declarations: [
     AppComponent,
-    MyfirstComponentComponent
+    MyfirstComponentComponent,
+    AboutComponentComponent
   ],
   imports: [
     BrowserModule,
